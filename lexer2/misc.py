759340afb9bc83f926ff46406147d91b20494018
DeltaRazero <deltarazero@gmail.com>
2020-11-19 11:26:07 +0100

    Set voidptr_t to ptr_t of typing.Any

--- lexer2/misc.py
+++ lexer2/misc.py
@@ -14,8 +14,9 @@ T = _t.TypeVar('T')
 
 # ***************************************************************************************
 
-# Typedef to simulate void*
-voidptr_t = _t.Any
-
 # Typedef to simulate pointer type
 ptr_t = _t.Union[T, None]
+
+# Typedef to simulate void*
+# voidptr_t = _t.Any
+voidptr_t = ptr_t[_t.Any]
